9d8f49b54e30946f812a8d384c52e4b4cf693a68
Veronica Enamorado <veronica.enamorado@gmail.com>
2019-08-22 09:25:42 +0200

    Add changingBg function

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 console.log("Listo!")
+
+const background = document.querySelector('.background');
+
+let test = document.getElementById("body").scrollHeight;
+let mitadScroll = test/2;
+  
+    
+const changePic = () => {
+  if ( window.scrollY >= mitadScroll) {
+    background.classList.remove('bg1');
+    background.classList.add('bg2');
+  } else {
+    background.classList.remove('bg2');
+    background.classList.add('bg1');
+  }
+}
+
+window.addEventListener('scroll', changePic);
