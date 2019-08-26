f84fb8dd0b3fa83aa32700c34412f3f5d3cd9696
Veronica Enamorado <veronica.enamorado@gmail.com>
2019-08-26 11:13:03 +0200

    Add preload, title after and different transitions

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 'use strict';
 
-console.log("Listo!")
+// ----------------------- CHANGE IMAGE ON SCROLL POSITION -------------------------------------
 
 const background = document.querySelector('.background');
-
 let test = document.getElementById("body").scrollHeight;
 let mitadScroll = test/2;
   
@@ -18,21 +17,10 @@ const changePic = () => {
   }
 }
 
-
-function preload(images) {
-  if (document.images) {
-      var i = 0;
-      var imageArray = new Array();
-      imageArray = images.split(',');
-      var imageObj = new Image();
-      for(i=0; i<=imageArray.length-1; i++) {
-          //document.write('<img src="' + imageArray[i] + '" />');// Write to page (uncomment to check images)
-          imageObj.src=imageArray[i];
-      }
-  }
-  console.log(images)
-}
-
-preload('/images/maxime-caron-iVhmp3iQJPA-unsplash.jpg, /images/max-zhiltsov-RB9VeeP8bsg-unsplash.jpg');
-
 window.addEventListener('scroll', changePic);
+
+
+// ----------------------- IMAGE PRELOAD -------------------------------------
+
+
+
